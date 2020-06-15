5fa8a3ea4ca2bbe9122b5546977fbc6cefbd10ab
Klay <klaykulik@gmail.com>
2020-06-15 16:57:44 -0400

    bayes stuff

--- pymc3_test.py
+++ pymc3_test.py
@@ -1,122 +1,126 @@
-import pymc3 as pm
-
-with pm.Model() as model:
-    parameter = pm.Exponential("poisson_param", 1.0)
-    data_generator = pm.Poisson("data_generator", parameter)
-
-
-
-with model:
-    data_plus_one = data_generator + 1
-
-
-with pm.Model() as model:
-    theta = pm.Exponential("theta", 2.0)
-    data_generator = pm.Poisson("data_generator", theta)
-
-
-
-
-with pm.Model() as ab_testing:
-    p_A = pm.Uniform("P(A)", 0, 1)
-    p_B = pm.Uniform("P(B)", 0, 1)
-
-
-print("parameter.tag.test_value =", parameter.tag.test_value)
-print("data_generator.tag.test_value =", data_generator.tag.test_value)
-print("data_plus_one.tag.test_value =", data_plus_one.tag.test_value)
-
-with pm.Model() as model:
-    parameter = pm.Exponential("poisson_param", 1.0, testval=0.5)
-
-print("\nparameter.tag.test_value =", parameter.tag.test_value)
-
-
-
-with pm.Model() as model:
-    lambda_1 = pm.Exponential("lambda_1", 1.0)
-    lambda_2 = pm.Exponential("lambda_2", 1.0)
-    tau = pm.DiscreteUniform("tau", lower=0, upper=10)
-
-new_deterministic_variable = lambda_1 + lambda_2
-
-
-import numpy as np
-
-n_data_points = 5  # in CH1 we had ~70 data points
-idx = np.arange(n_data_points)
-with model:
-    lambda_ = pm.math.switch(tau >= idx, lambda_1, lambda_2)
-
-
-import theano.tensor as tt
-
-with pm.Model() as theano_test:
-    p1 = pm.Uniform("p", 0, 1)
-    p2 = 1 - p1
-    p = tt.stack([p1, p2])
-
-    assignment = pm.Categorical("assignment", p)
-
-
-
-from IPython.core.pylabtools import figsize
-import matplotlib.pyplot as plt
 import scipy.stats as stats
-# figsize(12.5, 4)
+from IPython.core.pylabtools import figsize
+import numpy as np
+figsize(12.5, 4)
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+jet = plt.cm.jet
+
+x = np.linspace(0, 1, 100)
+y = np.linspace(0.5, 1.5, 100)
+X, Y = np.meshgrid(x, y)
+
+fig = plt.figure()
+plt.subplot(121)
+uni_x = stats.uniform.pdf(x, loc=0, scale=5)
+uni_y = stats.uniform.pdf(y, loc=0, scale=5)
+M = np.dot(uni_x[:, None], uni_y[None, :])
+im = plt.imshow(M, interpolation='none', origin='lower',
+                cmap=jet, vmax=1, vmin=-.15, extent=(0, 5, 0, 5))
+
+plt.xlim(0, 5)
+plt.ylim(0, 5)
+plt.title("Landscape formed by Uniform priors.")
+
+ax = fig.add_subplot(122, projection='3d')
+ax.plot_surface(X, Y, M, cmap=plt.cm.jet, vmax=1, vmin=-.15)
+ax.view_init(azim=390)
+plt.title("Uniform prior landscape; alternate view");
+plt.show()
+
+# create the observed data
+
+# sample size of data we observe, trying varying this (keep it less than 100 ;)
+N = 5
+
+# the true parameters, but of course we do not see these values...
+lambda_1_true = 1.0
+lambda_2_true = 0.5
+
+#...we see the data generated, dependent on the above two values.
+data = np.concatenate([
+    stats.poisson.rvs(lambda_1_true, size=(N, 1)),
+    stats.poisson.rvs(lambda_2_true, size=(N, 1))
+], axis=1)
+print("observed (2-dimensional,sample size = %d):" % N, data)
+
+# plotting details.
+# x = y = np.linspace(.01, 5, 100)
+likelihood_x = np.array([stats.poisson.pmf(data[:, 0], _x)
+                        for _x in x]).prod(axis=1)
+likelihood_y = np.array([stats.poisson.pmf(data[:, 1], _y)
+                        for _y in y]).prod(axis=1)
+L = np.dot(likelihood_x[:, None], likelihood_y[None, :])
 
 
-# samples = lambda_1.random(size=20000)
-# plt.hist(samples, bins=70, normed=True, histtype="stepfilled")
-# plt.title("Prior distribution for $\lambda_1$")
-# plt.xlim(0, 8);
-# plt.show()
+figsize(12.5, 12)
+# matplotlib heavy lifting below, beware!
+plt.subplot(221)
+uni_x = stats.uniform.pdf(x, loc=0, scale=5)
+uni_y = stats.uniform.pdf(x, loc=0, scale=5)
+M = np.dot(uni_x[:, None], uni_y[None, :])
+im = plt.imshow(M, interpolation='none', origin='lower',
+                cmap=jet, vmax=1, vmin=-.15, extent=(0, 1, 0.5, 1.5))
+plt.scatter(lambda_2_true, lambda_1_true, c="k", s=50, edgecolor="none")
+plt.xlim(0, 1)
+plt.ylim(0.5, 1.5)
+plt.title("Landscape formed by Uniform priors on $p_1, p_2$.")
 
-# data = np.array([10, 5])
-# with model:
-#     fixed_variable = pm.Poisson("fxd", 1, observed=data)
-# print("value: ", fixed_variable.tag.test_value)
+plt.subplot(223)
+plt.contour(x, y, M * L)
+im = plt.imshow(M * L, interpolation='none', origin='lower',
+                cmap=jet, extent=(0, 1, 0.5, 1.5))
+plt.title("Landscape warped by %d data observation;\n Uniform priors on $p_1, p_2$." % N)
+plt.scatter(lambda_2_true, lambda_1_true, c="k", s=50, edgecolor="none")
+plt.xlim(0, 1)
+plt.ylim(0.5, 1.5)
+
+plt.subplot(222)
+exp_x = stats.expon.pdf(x, loc=0, scale=3)
+exp_y = stats.expon.pdf(x, loc=0, scale=10)
+M = np.dot(exp_x[:, None], exp_y[None, :])
+
+plt.contour(x, y, M)
+im = plt.imshow(M, interpolation='none', origin='lower',
+                cmap=jet, extent=(0, 1, 0.5, 1.5))
+plt.scatter(lambda_2_true, lambda_1_true, c="k", s=50, edgecolor="none")
+plt.xlim(0, 1)
+plt.ylim(0.5, 1.5)
+plt.title("Landscape formed by Exponential priors on $p_1, p_2$.")
+
+plt.subplot(224)
+# This is the likelihood times prior, that results in the posterior.
+plt.contour(x, y, M * L)
+im = plt.imshow(M * L, interpolation='none', origin='lower',
+                cmap=jet, extent=(0, 1, 0.5, 1.5))
+
+plt.scatter(lambda_2_true, lambda_1_true, c="k", s=50, edgecolor="none")
+plt.title("Landscape warped by %d data observation;\n Exponential priors on \
+$p_1, p_2$." % N)
+plt.xlim(0, 1)
+plt.ylim(0.5, 1.5)
 
 
-# # We're using some fake data here
-# data = np.array([10, 25, 15, 20, 35])
-# with model:
-#     obs = pm.Poisson("obs", lambda_, observed=data)
-# print(obs.tag.test_value)
 
 
-# tau = np.random.randint(0, 80)
-# print(tau)
-
-# alpha = 1. / 20.
-# lambda_1, lambda_2 = np.random.exponential(scale=1 / alpha, size=2)
-# print(lambda_1, lambda_2)
-
-# data = np.r_[stats.poisson.rvs(mu=lambda_1, size=tau), stats.poisson.rvs(mu=lambda_2, size=80 - tau)]
-
-# plt.bar(np.arange(80), data, color="#348ABD")
-# plt.bar(tau - 1, data[tau - 1], color="r", label="user behaviour changed")
-# plt.xlabel("Time (days)")
-# plt.ylabel("count of text-msgs received")
-# plt.title("Artificial dataset")
-# plt.xlim(0, 80)
-# plt.legend()
-# plt.show()
 
 
-def plot_artificial_sms_dataset():
-    tau = stats.randint.rvs(0, 80)
-    alpha = 1./20.
-    lambda_1, lambda_2 = stats.expon.rvs(scale=1/alpha, size=2)
-    data = np.r_[stats.poisson.rvs(mu=lambda_1, size=tau), stats.poisson.rvs(mu=lambda_2, size=80 - tau)]
-    plt.bar(np.arange(80), data, color="#348ABD")
-    plt.bar(tau - 1, data[tau-1], color="r", label="user behaviour changed")
-    plt.xlim(0, 80);
 
-figsize(12.5, 5)
-plt.title("More example of artificial datasets")
-for i in range(4):
-    plt.subplot(4, 1, i+1)
-    plot_artificial_sms_dataset()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 plt.show()
--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
-
+import astropy.constants as cst
+import scipy.stats as stats
 
 from edibles.utils.edibles_spectrum import EdiblesSpectrum
-from edibles.models.create_model import createCont
-from edibles.models.model import Sightline
-from edibles.fitter import fit
 
-from wavelength_corr import correctWavelength
+from interpolate import interpolate
 
 
 FILE1 = "/HD170740/RED_860/HD170740_w860_redl_20140915_O12.fits"
@@ -17,114 +14,141 @@ FILE3 = "/HD170740/RED_860/HD170740_w860_redl_20150626_O12.fits"
 FILE4 = "/HD170740/RED_860/HD170740_w860_redl_20160613_O12.fits"
 FILE5 = "/HD170740/RED_860/HD170740_w860_redl_20170705_O12.fits"
 
+xmin = 7661.5
+xmax = 7669
 
 sp1 = EdiblesSpectrum(FILE1)
-subset1 = sp1.getSpectrum(xmin=7661.5, xmax=7669)
-# subset1.flux = subset1.flux / np.max(subset1.flux)
-prominence1 = (np.max(subset1.flux) - np.min(subset1.flux)) * 0.1
-peaks1, _ = find_peaks(-subset1.flux, prominence=prominence1)
-peak_wavelengths1 = [subset1.wave.iloc[i] for i in peaks1]
-peak_fluxes1 = [subset1.flux.iloc[i] for i in peaks1]
-
-
+subset1 = sp1.getSpectrum(xmin=xmin, xmax=xmax)
 sp2 = EdiblesSpectrum(FILE2)
-subset2 = sp2.getSpectrum(xmin=7661.5, xmax=7669)
-# subset2.flux = subset2.flux / np.max(subset2.flux)
-prominence2 = (np.max(subset2.flux) - np.min(subset2.flux)) * 0.1
-peaks2, _ = find_peaks(-subset2.flux, prominence=prominence2)
-peak_wavelengths2 = [subset2.wave.iloc[i] for i in peaks2]
-peak_fluxes2 = [subset2.flux.iloc[i] for i in peaks2]
-
-
+subset2 = sp2.getSpectrum(xmin=xmin, xmax=xmax)
 sp3 = EdiblesSpectrum(FILE3)
-subset3 = sp3.getSpectrum(xmin=7661.5, xmax=7669)
-# subset3.flux = subset3.flux / np.max(subset3.flux)
-prominence3 = (np.max(subset3.flux) - np.min(subset3.flux)) * 0.1
-peaks3, _ = find_peaks(-subset3.flux, prominence=prominence3)
-peak_wavelengths3 = [subset3.wave.iloc[i] for i in peaks3]
-peak_fluxes3 = [subset3.flux.iloc[i] for i in peaks3]
-
-
+subset3 = sp3.getSpectrum(xmin=xmin, xmax=xmax)
 sp4 = EdiblesSpectrum(FILE4)
-subset4 = sp4.getSpectrum(xmin=7661.5, xmax=7669)
-# subset4.flux = subset4.flux / np.max(subset4.flux)
-prominence4 = (np.max(subset4.flux) - np.min(subset4.flux)) * 0.1
-peaks4, _ = find_peaks(-subset4.flux, prominence=prominence4)
-peak_wavelengths4 = [subset4.wave.iloc[i] for i in peaks4]
-peak_fluxes4 = [subset4.flux.iloc[i] for i in peaks4]
-
-
+subset4 = sp4.getSpectrum(xmin=xmin, xmax=xmax)
 sp5 = EdiblesSpectrum(FILE5)
-subset5 = sp5.getSpectrum(xmin=7661.5, xmax=7669)
-# subset5.flux = subset5.flux / np.max(subset5.flux)
-prominence5 = (np.max(subset5.flux) - np.min(subset5.flux)) * 0.1
-peaks5, _ = find_peaks(-subset5.flux, prominence=prominence5)
-peak_wavelengths5 = [subset5.wave.iloc[i] for i in peaks5]
-peak_fluxes5 = [subset5.flux.iloc[i] for i in peaks5]
+subset5 = sp5.getSpectrum(xmin=xmin, xmax=xmax)
+
+spectra = [sp1, sp2, sp3, sp4, sp5]
+iwave, fluxes = interpolate(spectra, xmin=xmin, xmax=xmax)
 
 
-plt.plot(subset1.wave, subset1.flux)
-plt.scatter(peak_wavelengths1, peak_fluxes1)
-
-plt.plot(subset2.wave, subset2.flux)
-plt.scatter(peak_wavelengths2, peak_fluxes2)
-
-plt.plot(subset3.wave, subset3.flux)
-plt.scatter(peak_wavelengths3, peak_fluxes3)
-
-plt.plot(subset4.wave, subset4.flux)
-plt.scatter(peak_wavelengths4, peak_fluxes4)
-
-plt.plot(subset5.wave, subset5.flux)
-plt.scatter(peak_wavelengths5, peak_fluxes5)
-
-plt.show()
+for i in range(len(fluxes)):
+    fluxes[i] = fluxes[i] / np.median(fluxes[i])
 
 
-y_vlist1 = np.ones_like(peak_wavelengths1) * sp1.v_bary
-y_vlist2 = np.ones_like(peak_wavelengths2) * sp2.v_bary
-y_vlist3 = np.ones_like(peak_wavelengths3) * sp3.v_bary
-y_vlist4 = np.ones_like(peak_wavelengths4) * sp4.v_bary
-y_vlist5 = np.ones_like(peak_wavelengths5) * sp5.v_bary
+# [plt.plot(iwave, flux, marker='.') for flux in fluxes]
+# plt.show()
 
 
-plt.scatter(peak_wavelengths1, y_vlist1, label=sp1.date[0:10])
-plt.scatter(peak_wavelengths2, y_vlist2, label=sp2.date[0:10])
-plt.scatter(peak_wavelengths3, y_vlist3, label=sp3.date[0:10])
-plt.scatter(peak_wavelengths4, y_vlist4, label=sp4.date[0:10])
-plt.scatter(peak_wavelengths5, y_vlist5, label=sp5.date[0:10])
+# ######################## first point
 
-plt.xlabel('Peak Wavelength')
-plt.ylabel('Barycentric Velocity')
-plt.legend()
+# ### MODEL
+def model(flux, tell):
+    '''
+    Args:
+        flux (float): The observed flux value
+        tell (float): The telluric transmission, 0<tell<1
+
+    Returns:
+        cont (float): The flux before passing through the atmosphere
+    '''
+    cont = tell / flux
+    return cont
 
 
-plt.axvline(x=peak_wavelengths2[0])
-plt.axvline(x=peak_wavelengths2[-1])
+# ### PRIOR - probability model is correct, or, what we think the initial values should be
+fig, axs = plt.subplots(1, 2, sharey=True)
+plt.suptitle("Priors")
 
+# Tell should be between 0 and 1 - represents transmission coefficient
+# lets say uniform for now?
+telluric = np.linspace(0, 1)
+tell_prob = stats.uniform.pdf(telluric)
+axs[0].plot(telluric, tell_prob)
+axs[0].fill_between(telluric, 0, tell_prob, alpha=0.3)
+axs[0].set_ylabel("PDF")
+axs[0].set_xlabel("Telluric Transmission")
 
-linex = [peak_wavelengths1[1], peak_wavelengths2[1], peak_wavelengths3[0], peak_wavelengths4[0], peak_wavelengths5[0]]
-liney = [sp1.v_bary, sp2.v_bary, sp3.v_bary, sp4.v_bary, sp5.v_bary]
-
-z = np.polyfit(linex, liney, 1)
-p = np.poly1d(z)
-plt.plot(linex, p(linex))
-
+# cont_flux doesnt really have a necessary range - probably near 1
+# I DONT THINK YOU NEED THIS...
+cont_flux = np.linspace(0, 1)
+cont_prob = stats.beta.pdf(cont_flux, 4, 2)
+cont_flux += 0.2
+axs[1].plot(cont_flux, cont_prob)
+axs[1].fill_between(cont_flux, 0, cont_prob, alpha=0.3)
+axs[1].set_xlabel("Continuum value")
 
 plt.show()
 
 
-datax = peak_wavelengths1 + peak_wavelengths2 + peak_wavelengths3 + peak_wavelengths4 + peak_wavelengths5
-datay = list(y_vlist1) + list(y_vlist2) + list(y_vlist3) + list(y_vlist4) + list(y_vlist5)
-classification = ['blue', 'red', 'blue', 'blue', 'red', 'blue', 'red', 'blue', 'blue', 'red', 'blue', 'blue', 'red', 'blue', 'blue']
-c_num = [0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0]
+# ### LIKELIHOOD - probability you get the data, GIVEN THE MODEL IS CORRECT
 
-print(datax)
-print(datay)
+data = [flux[0] for flux in fluxes]
+print(data)
+tell = 1
 
-plt.scatter(datax, datay, color=classification)
+likes = []
+[likes.append(model(datum, tell)) for datum in data]
+print(likes)
+
+fig, axs = plt.subplots(1, 2, sharey=True)
+plt.suptitle("Likelihoods")
+
+
+telluric = np.linspace(0, 1)
+tell_prob = stats.uniform.pdf(telluric)
+axs[0].plot(telluric, tell_prob)
+axs[0].fill_between(telluric, 0, tell_prob, alpha=0.3)
+axs[0].vlines(tell, ymin=0, ymax=1, color='r')
+axs[0].set_ylabel("PDF")
+axs[0].set_xlabel("Telluric Transmission")
+
+
+cont_flux = np.linspace(0, 1)
+cont_prob = stats.beta.pdf(cont_flux, 4, 2)
+cont_flux += 0.2
+axs[1].plot(cont_flux, cont_prob)
+axs[1].fill_between(cont_flux, 0, cont_prob, alpha=0.3)
+axs[1].vlines(likes, ymin=0, ymax=2, color='red')
+
+# axs[1].set_xlabel("Continuum value")
+
 plt.show()
 
+# ### posterior
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # ##### tell ref frame
+
+# iwaves_bary = []
+# for sp in data:
+#     v_bary = sp.v_bary
+
+#     iwave_bary = iwave[0] + (sp.v_bary / cst.c.to("km/s").value) * iwave[0]
+
+#     iwaves_bary.append(iwave_bary)
+
+
+# print(iwaves_bary)
+
+
+# plt.scatter(iwave[0], 1)
+# [plt.scatter(iwave_bary, 2) for iwave_bary in iwaves_bary]
+# plt.show()
