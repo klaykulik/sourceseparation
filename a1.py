cd4751052eb2a69b39c6b1c0c6cf3c5cd085074f
Klay <klaykulik@gmail.com>
2020-07-02 18:38:06 -0400

    deleted multinest stuff

--- a1.py
+++
@@ -1,9 +0,0 @@
-import pymultinest
-
-def prior(cube, ndim, nparams):
-    cube[0] = cube[0] * 2
-
-def loglikelihood(cube, ndim, nparams):
-    return -0.5 * ((cube[0] - 0.2) / 0.1)**2
-
-pymultinest.run(loglikelihood, prior, n_params=1, n_dims=1, outputfiles_basename='out/')
