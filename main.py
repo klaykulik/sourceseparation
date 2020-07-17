f6d8387075115e447ab408e9f4c5bf7926f7d402
Klay <klaykulik@gmail.com>
2020-07-17 15:26:31 -0400

    can fit all telluric lines

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from edibles.utils.edibles_spectrum import EdiblesSpectrum
 from edibles.sightline import Sightline
 
 from sourceseparation.wavelength_corr import correct
+from sourceseparation.read_hitran import read_hitran, convert
 
 file1 = "/HD170740/RED_860/HD170740_w860_redl_20140915_O12.fits"
 file2 = "/HD170740/RED_860/HD170740_w860_redl_20140916_O12.fits"
@@ -14,8 +15,8 @@ file3 = "/HD170740/RED_860/HD170740_w860_redl_20150626_O12.fits"
 file4 = "/HD170740/RED_860/HD170740_w860_redl_20160613_O12.fits"
 file5 = "/HD170740/RED_860/HD170740_w860_redl_20170705_O12.fits"
 
-xmin = 7661.5
-xmax = 7669.0
+xmin = 7642
+xmax = 7680
 
 
 sp1 = EdiblesSpectrum(file1)
@@ -31,61 +32,100 @@ sp5.getSpectrum(xmin, xmax)
 observations = [sp1, sp2, sp3, sp4, sp5]
 
 
-for spec in observations:
-    plt.plot(spec.wave, spec.flux)
-# plt.show()
+sightline1 = Sightline(sp1, n_anchors=10)
+sightline2 = Sightline(sp2)
+sightline3 = Sightline(sp3)
+sightline4 = Sightline(sp4)
+sightline5 = Sightline(sp5)
+
+sightlines = [sightline1, sightline2, sightline3, sightline4, sightline5]
 
 
-# observations = correct(observations)
+out = sightline1.model.eval(data=sp1.flux, params=sightline1.model_pars, x=sp1.wave)
+resid = sp1.flux - out
 
-
-# for spec in observations:
-#     plt.plot(spec.wave, spec.flux)
-# plt.show()
-
-
-sightlines = []
-for spec in observations:
-
-    sightline = Sightline(spec)
-    sightlines.append(sightline)
-
-
-
-
-print(sightlines)
+plt.plot(sp1.wave, sp1.flux)
+plt.plot(sp1.wave, out)
+plt.plot(sp1.wave, resid)
+plt.show()
 
 
 # O_2 data from HITRAN
-wavenums = [13041.123638, 13042.947272]
-wavelens = [u.AA / (wn / 1e8) for wn in wavenums]
-print(wavelens)
+pars_list = convert(read_hitran('telluric_lines_HITRAN.txt'))
 
-air_waves = [vac_to_air(wl, method='Ciddor1996') for wl in wavelens]
+# for pars in pars_list:
+#     print(pars)
+
+print(pars_list[0])
+
+linelist = []
+for pars in pars_list:
+    if (xmin < pars['lam_0']) and (pars['lam_0'] < xmax):
+
+        linelist.append(pars)
 
 
 
-print(air_waves)
 
 
 
-for air_wave in air_waves:
-    plt.scatter(air_wave, 5000)
+plt.plot(sp1.wave, sp1.flux)
+
+
+tau_cutoff = 0.00
+
+
+
+for pars in linelist:
+
+    if pars['tau_0'] > tau_cutoff:
+        print(pars)
+        plt.scatter(pars['lam_0'], 100)
 
 plt.show()
 
 
+sightline1.add_source(name='O2', similar={'b': 0.6})
+
+for i in range(len(linelist)):
+    line = linelist[i]
+    if line['tau_0'] > tau_cutoff:
+        name = 'line' + str(i)
+        sightline1.add_line(name=name, source='O2', pars=line)
+        # print(sightline1.model_pars)
+        par_name_d = 'O2_' + name + '_d'
+        sightline1.model_pars[par_name_d].set(value=0.05)
+
+out = sightline1.model.eval(data=sp1.flux, params=sightline1.model_pars, x=sp1.wave)
+resid = sp1.flux - out
+
+plt.plot(sp1.wave, sp1.flux)
+plt.plot(sp1.wave, out)
+plt.plot(sp1.wave, resid)
+plt.show()
+
+
+sightline1.fit(report=True, plot=True)
 
 
 
+out = sightline1.model.eval(data=sp1.flux, params=sightline1.result.params, x=sp1.wave)
+resid = sp1.flux - out
+
+plt.plot(sp1.wave, sp1.flux)
+plt.plot(sp1.wave, out)
+plt.plot(sp1.wave, resid)
 
 
 
+for pars in linelist:
 
+    if pars['tau_0'] > tau_cutoff:
 
+        if pars['tau_0'] == 0.5:
+            plt.scatter(pars['lam_0'], 100, marker='.')
 
+        else:
+            plt.scatter(pars['lam_0'], 1500, marker='.')
 
-
-
-
-
+plt.show()
